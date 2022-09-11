58ce706cf5946bc91fd2d41aac8fec7757bcd050
Yousef Omar <yousefomar724@gmail.com>
2022-09-11 14:53:23 +0200

    add animation - edit

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,10 +6,10 @@ const showMenu = (toggleId, navId) => {
   if (toggle && menu) {
     toggle.addEventListener('click', () => {
       menu.classList.toggle('show')
+      document.body.classList.toggle('hide')
     })
   }
 }
-
 showMenu('menu-icon', 'menu')
 
 /* Remove menu */
@@ -18,37 +18,7 @@ const navLink = document.querySelectorAll('.menu__link'),
 
 function linkAction() {
   navMenu.classList.remove('show')
+  document.body.classList.remove('hide')
 }
 
 navLink.forEach((n) => n.addEventListener('click', linkAction))
-
-/* Scroll Section  Active Header */
-const sections = document.querySelectorAll('section[id]')
-window.addEventListener('scroll', scrollActive)
-
-function scrollActive() {
-  const scrollY = window.pageYOffset
-
-  sections.forEach((current) => {
-    const sectionHeight = current.offsetHeight
-    const sectionTop = current.offsetTop - 50
-    sectionId = current.getAttribute('id')
-
-    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      document
-        .querySelector(`.nav__menu a[href*=${sectionId}]`)
-        .classList.add('active')
-    } else {
-      document
-        .querySelector(`.nav__menu a[href*=${sectionId}]`)
-        .classList.remove('active')
-    }
-  })
-}
-
-/* Change Color Header */
-window.onscroll = () => {
-  const nav = document.getElementById('header')
-  if (this.scrollY >= 200) nav.classList.add('scroll-header')
-  else nav.classList.remove('scroll-header')
-}
